61227b9ad5aea34f0544a0e13d53c994f6956e3b
Dineshdini71 <dinidinesh71@gmail.com>
2024-07-07 20:35:07 +0530

    Removed bucket2&3 && DB

--- flowlogs-s3.tf
+++ flowlogs-s3.tf
@@ -5,27 +5,27 @@ resource "aws_s3_bucket" "terraform_flowlogs_bucket1" {
     Name        = "terraformflowlogsbucket1"
     Environment = "Dev"
   }
-  depends_on = [aws_s3_bucket.terraform_flowlogs_bucket2]
+  # depends_on = [aws_s3_bucket.terraform_flowlogs_bucket2]
   lifecycle {
     prevent_destroy       = false
     create_before_destroy = true
   }
 }
 
-resource "aws_s3_bucket" "terraform_flowlogs_bucket2" {
-  bucket = "terraformflowlogsbucket2"
-  tags = {
-    Name        = "terraformflowlogsbucket2"
-    Environment = "Dev"
-  }
-  depends_on = [aws_s3_bucket.terraform_flowlogs_bucket3]
-}
+# resource "aws_s3_bucket" "terraform_flowlogs_bucket2" {
+#   bucket = "terraformflowlogsbucket2"
+#   tags = {
+#     Name        = "terraformflowlogsbucket2"
+#     Environment = "Dev"
+#   }
+#   depends_on = [aws_s3_bucket.terraform_flowlogs_bucket3]
+# }
 
-resource "aws_s3_bucket" "terraform_flowlogs_bucket3" {
-  bucket = "terraformflowlogsbucket3"
-  tags = {
-    Name        = "terraformflowlogsbucket3"
-    Environment = "Dev"
-  }
-  depends_on = [aws_route_table_association.terraform-public-1]
-}
+# resource "aws_s3_bucket" "terraform_flowlogs_bucket3" {
+#   bucket = "terraformflowlogsbucket3"
+#   tags = {
+#     Name        = "terraformflowlogsbucket3"
+#     Environment = "Dev"
+#   }
+#   depends_on = [aws_route_table_association.terraform-public-1]
+# }
